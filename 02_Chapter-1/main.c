680124b244de787c0082c44467758a5a6d9c2b1f
Deep Kansara <deepkansara1212@gmail.com>
2026-03-05 11:27:22 +0530

    C Variables

---
+++ 02_Chapter-1/main.c
@@ -0,0 +1,34 @@
+// Variables
+// Data Type => int (%d), float (%f), char(%c)
+
+#include<stdio.h>
+
+int main() {
+    // Variable Declaration / Initialization
+    int number = 10;
+    int Number = 30;
+
+    int num1;
+    num1 = 20;
+
+    int Num2 = 30, num3 = 40, num4 = 50;
+
+    float num2 = 50;
+
+    char name = 'a';
+    char num = '1';
+
+    char a = 65;
+
+    printf("%c\n", a);
+
+    printf("%c\n", num);
+    printf("%c\n", name);
+
+    printf("%.2f\n", num2);
+
+    printf("%d\n", number);
+
+    printf("%d\n", num1);
+    return 0;
+}
