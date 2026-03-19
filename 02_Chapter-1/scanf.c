db26bb1ec3b0f4e63eb819249fa114e9bf750d7f
DeepKansara1212 <deepkansara1212@gmail.com>
2026-03-19 11:37:18 +0530

    Add C Switch Case

---
+++ 02_Chapter-1/scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter the value of num: ");
+    scanf("%d", &num);
+
+    printf("Value of num is %d", num);
+    return 0;
+}
---
+++ 03_Conditionals/06_Switch.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int day;
+
+    printf("Enter the value of day: ");
+    scanf("%d", &day);
+
+    switch (day)
+    {
+    case 1:
+        printf("MONDAY\n");
+        break;
+    case 2:
+        printf("TUESDAY\n");
+        break;
+    case 3:
+        printf("WEDNESDAY\n");
+        break;
+    case 4:
+        printf("THURSDAY\n");
+        break;
+    case 5:
+        printf("FRIDAY\n");
+        break;
+    case 6:
+        printf("SATURDAY\n");
+        break;
+    case 7:
+        printf("SUNDAY\n");
+        break;
+    default:
+        printf("Enter the valid day between 1 to 7.");
+        break;
+    }
+
+    return 0;
+}
