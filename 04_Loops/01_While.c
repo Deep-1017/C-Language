7621483eb70af392c0040c46761e2c45d90ac880
DeepKansara1212 <deepkansara1212@gmail.com>
2026-03-20 11:43:31 +0530

    Add C Loops

---
+++ 04_Loops/01_While.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    // printf("1");
+    // printf("2");
+    // printf("3");
+    // printf("4");
+    // printf("5");
+    // printf("6");
+    // printf("7");
+    // printf("8");
+    // printf("9");
+    // printf("10");
+
+    int num = 1;
+
+    while (num <= 10)
+    {
+        printf("%d\n", num);
+        num = num + 1;
+    }
+
+    return 0;
+}
