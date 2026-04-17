7ccc50816d9c85a7d07ecf116e0a45342e2da5d4
DeepKansara1212 <deepkansara1212@gmail.com>
2026-04-17 11:39:23 +0530

    Add C Array

--- 06_Arrays/01_main.c
+++ 06_Arrays/01_main.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 
-int main() {
-    
+int main()
+{
     int num;
     num = 10;
 
     int numbers[5] = {10, 20, 30, 40, 50};
-    printf("Before update: %d\n", numbers[2]);
+    // printf("Before update: %d\n", numbers[2]);
 
     numbers[2] = 25;
-    printf("After update: %d\n", numbers[2]);
+    // printf("After update: %d\n", numbers[2]);
 
     char words[] = {'a', 'b', 'c', 'd', 'e'};
+    // printf("%c\n", words[0]);
+    // printf("%c\n", words[1]);
+    // printf("%c\n", words[2]);
+    // printf("%c\n", words[3]);
+    // printf("%c\n", words[4]);
+
+    int length;
+    length = sizeof(words) / sizeof(words[0]);
+    // printf("%d\n", length);
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("%c\n", words[i]);
+    }
+    
 
     return 0;
 }
