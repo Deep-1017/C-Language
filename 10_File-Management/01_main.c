c119d3bdfba611cf97c4861b029c3459b9f0d00d
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-02 10:25:52 +0530

    Add C File Management

--- 10_File-Management/01_main.c
+++ 10_File-Management/01_main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    int num;
+    int num_wrt = 500;
+
+    char str[100] = "Hello World!";
 
     FILE *ptr;
-    ptr = fopen("demo.txt", "r");
+    // ptr = fopen("demo.txt", "r");
+    // fscanf(ptr, "%d", &num);
+    // printf("Number: %d\n", num);
+
+    ptr = fopen("demo.txt", "a");
+    fprintf(ptr, "%d", num_wrt);
+    fprintf(ptr, " %s", str);
+    fclose(ptr);
+
     return 0;
 }
